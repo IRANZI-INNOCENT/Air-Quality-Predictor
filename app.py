e374635265a6d2ba96e2738e4240c2f476a2e64e
IRANZI-INNOCENT <160141125+IRANZI-INNOCENT@users.noreply.github.com>
2024-03-31 14:55:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,43 @@ import streamlit as st
 import pandas as pd
 from aqi_predict import predict_input
 
-st.title('AIR QUALITY INDEX PREDICTION')
+st.title('AQI PREDICTION MODEL')
 st.markdown('Predict the Air Quality Index (AQI) with accuracy. Set your input parameters below.')
 
 # Input fields
-input1 = {'co': 0.100,
-          'no': 0.0003,
-          'no2': 0.001,
-          'o3': 0.000,
-          'pm1': 0.000,
-          'pm10': 0.150,
-          'pm25': 0.000,
-          'so2': 0.000,
-          'temperature': 5}
+input1 = {'co':'500',
+          'no':'0.07',
+          'no2':'80',
+          'o3':'160',
+          'pm1':'100',
+          'pm10':'170',
+          'pm25':'500',
+          'so2':'30',
+          'temperature':'27'}
 
-st.header("AIR QUALITY PREDICTORS")
+st.header("AIR QUALITY PARAMETERS")
 
 # Create input fields with 3 columns
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.text("ATMOSPHERIC AIR")
-    co = st.number_input('CO (µg/m³)', min_value=0.100, max_value=1000, step=0.001, value=0.100)
-    no = st.number_input('NO (ppm)', min_value=0.0003, max_value=0.0001, step=0.0001, value=0.0003)
-    no2 = st.number_input('NO2 (µg/m³)', min_value=0.001, max_value=200, step=0.001, value=0.001)
+    co = st.number_input('CO (µg/m³)', min_value=0, max_value=1000, step=1, value=500)
+    no = st.number_input('NO (ppm)', min_value=0.0, max_value=1.0, step=0.01, value=0.07)
+    no2 = st.number_input('NO2 (µg/m³)', min_value=0, max_value=200, step=1, value=80)
+    
 
 with col2:
     st.text("PARTICULATE MATTERS")
-    pm1 = st.number_input('PM1 (µg/m³)', min_value=0.000, max_value=1000, step=0.001, value=0.000)
-    pm10 = st.number_input('PM10 (µg/m³)', min_value=0.150, max_value=1000, step=0.050, value=0.150)
-    pm25 = st.number_input('PM2.5 (µg/m³)', min_value=0.000, max_value=1000, step=0.001, value=0.000)
+    pm1 = st.number_input('PM1 (µg/m³)', min_value=0, max_value=1000, step=1, value=100)
+    pm10 = st.number_input('PM10 (µg/m³)', min_value=0, max_value=1000, step=1, value=170)
+    pm25 = st.number_input('PM2.5 (µg/m³)', min_value=0, max_value=1000, step=1, value=500)
 
 with col3:
     st.text("OTHER PARAMETERS")
-    so2 = st.number_input('SO2 (µg/m³)', min_value=0.000, max_value=100, step=0.001, value=0.000)
-    temperature = st.number_input('Temperature (°C)', min_value=-20, max_value=50, step=1, value=5)
-    o3 = st.number_input('O3 (µg/m³)', min_value=0.000, max_value=200, step=0.001, value=0.000)
+    so2 = st.number_input('SO2 (µg/m³)', min_value=0, max_value=100, step=1, value=30)
+    temperature = st.number_input('Temperature (°C)', min_value=-20, max_value=50, step=1, value=27)
+    o3 = st.number_input('O3 (µg/m³)', min_value=0, max_value=200, step=1, value=160)
 
 # Update input data with user inputs
 input1.update({
@@ -66,4 +67,4 @@ if st.button("Predict AQI"):
 
 st.text('')
 st.text('')
-st.markdown('Developed By: IRANZI INNOCENT | `Code:` [GitHub](https://github.com/IRANZI-INNOCENT/Data-Science)')
+st.markdown('Developed By: IRANZI INNOCENT vs MARGRET AMONDI | ` Code:` [GitHub](https://github.com/IRANZI-INNOCENT/Data-Sciennce)')
